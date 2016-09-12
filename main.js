4156313144b9b8b1bd41dd69649de1d7f756edfc
Andrew Biddle <abidds2@gmail.com>
2016-09-12 14:24:43 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let todos = [];
+//ALL DONE IN es6
+//have for for user to add todo item
+//todo item shows up in list
+//if user clicks item it gets marked complete or not complete
+//have a visual indicator of complete
+
+//BONUS
+//ability to delete todos
+//filter todos by complete / imcomplete / all
+//edit todo name
